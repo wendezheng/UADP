1d1496515e87eff9479cab3dabfad78c6a581b81
wendezheng <1261719755@qq.com>
2016-09-11 10:11:13 +0800

    Revert "实验二"
    This reverts commit fdce93d044760030cac862763ddcfefc658a734c.

--- UADP/CSUAppDelegate.h
+++ UADP/CSUAppDelegate.h
@@ -7,24 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "DBOperation.h"
-#import "sqlite3.h"
-@interface CSUAppDelegate : UIResponder <UIApplicationDelegate,UIAlertViewDelegate>
-{
-@public
-    sqlite3 *database;
-    DBOperation *dboperate;
-    NSDictionary *themeDictionary;
-}
+
+@interface CSUAppDelegate : UIResponder <UIApplicationDelegate>
+
 @property (strong, nonatomic) UIWindow *window;
-@property(strong,nonatomic)UINavigationController *inavigationController;
-@property(strong,nonatomic) NSString *username;
-@property(strong,nonatomic) NSString *password;
-@property(strong,nonatomic) NSString *account;
-@property(nonatomic) int uid;
-@property(nonatomic) int deptid;
-@property(nonatomic) int appid;
-@property(strong,nonatomic) NSString *deptname,*deptcode;
-@property(strong,nonatomic) NSDictionary *themeDictionary;
-@property(nonatomic,strong) DBOperation *dboperate;
+
 @end
